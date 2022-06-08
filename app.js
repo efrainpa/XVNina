de60197b3b244e77ff69e89ed6e33ecb164e52bf
efrainpa <91482821+efrainpa@users.noreply.github.com>
2022-06-08 11:52:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ function cargaDias(segundos, minutos, horas) {
 setInterval(cargarSegundo, 1000);
 
 function play(){
-    var audio = new Audio('musica/This is Me (Official Audio).mp4');
+    var audio = new Audio('musica/This Is Me (Official Audio).mp4');
     audio.play();
     var boton = document.getElementById('btn-play');
     boton.disabled=true;
