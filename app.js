6bc8323c2e3af0919a7b1ab0fa17994b27453596
efrainpa <91482821+efrainpa@users.noreply.github.com>
2022-06-08 11:35:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ function cargaDias(segundos, minutos, horas) {
 setInterval(cargarSegundo, 1000);
 
 function play(){
-    var audio = new Audio('musica/A Thousand Years.mp3');
+    var audio = new Audio('musica/This is Me (Official Audio).mp4');
     audio.play();
     var boton = document.getElementById('btn-play');
     boton.disabled=true;
